4c3abbe270fe6122316e97c71306bee3e1a9f458
Josh <Josh.Kozlowski81@outlook.com>
2024-08-30 07:13:17 -0700

    project creation

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace active911_technical_webapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
